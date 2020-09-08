f293b7d987f6d14031c076dac3470b7c20545b15
Thao Le <tythao1998@gmail.com>
2020-09-08 16:42:30 +0700

    commit third

---
+++ nbproject/chuc_nang/dang_nhap/dang_nhap.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<form>";
+echo "Tên đăng nhập:    ";
+echo "<input type=\"text\" name=\"ten_dang_nhap\"><br><br>";
+echo "Mật khẩu: ";
+echo "<input type=\"password\" name=\"mat_khau\">";
+echo "</form>";
+?>
+
